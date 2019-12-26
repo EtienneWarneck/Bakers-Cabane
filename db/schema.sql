7f925f786a130a011a2bbdda9430b884d571a2aa
Etienne Warneck <etienne.warneck@gmail.com>
2019-12-26 11:38:42 -0500

    git cache cleared

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ CREATE DATABASE pastries_db;
 USE pastries_db;
 
 CREATE TABLE pastries (
-  id int NOT NULL AUTO_INCREMENT,
+  id int AUTO_INCREMENT NOT NULL,
   pastry_name varchar(255) NOT NULL,
   devoured BOOLEAN DEFAULT FALSE,
   PRIMARY KEY (id)
